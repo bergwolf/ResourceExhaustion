47f3c7c7caccedcb2f9cb6286ed7e82af60a81d8
Clay Baenziger <cwb@clayb.net>
2018-05-20 21:09:24 -0400

    Pause to see effect of threds as they increase

--- java_docker_test/java/JavaThreadTest.java
+++ java_docker_test/java/JavaThreadTest.java
@@ -2,6 +2,12 @@ public class JavaThreadTest {
   public static void main(String[] args) {
     long tc = 0;
     while(true){
+      if (tc % 500 == 0) {
+        try {
+          Thread.sleep((long)100);
+        } catch(InterruptedException e) {
+        }
+      }
       new Thread(new Runnable(){
         public void run() {
           try {
