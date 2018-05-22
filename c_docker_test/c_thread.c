705fdc9024bb57009b479fadb48612c08c34f472
Clay Baenziger <cwb@clayb.net>
2018-05-22 09:27:49 -0400

    Update thread test for better visibility

--- c_docker_test/c_thread.c
+++ c_docker_test/c_thread.c
@@ -5,35 +5,56 @@
 #include <sys/types.h>
 #include <sys/syscall.h>
 
+#define handle_error(msg) \
+    do { perror(msg); sleep(10); exit(EXIT_FAILURE); } while (0)
+
 // A normal C function that is executed as a thread 
 // when its name is specified in pthread_create()
-void *myThreadFun(void *vargp)
+void *myThreadFun(void *i)
 {
-    pid_t myTID;
-    myTID = syscall(SYS_gettid);
-    printf("Thread ID: %i\n", myTID);
+    pid_t myTID = syscall(SYS_gettid);
+    pid_t myPID = getpid();
+    printf("%i: In Thread ID: %i, %i\n", myPID, *(int *)i, myTID);
+    if (i != NULL)
+      free(i);
     sleep(10);
     return NULL;
 }
  
 int main()
 {
+    pid_t myPID = getpid();
+    int i = 0;
+    int *arg = NULL;
     pthread_t thread_id;
-    pid_t myPID;
-    myPID = getpid();
+    pthread_attr_t tattr;
+
     printf("PID: %i\n", myPID);
     sleep(5);
-    printf("Before Thread\n");
-    int i = 0;
+    printf("%i: Before Thread\n", myPID);
+
+    if (0 != pthread_attr_init(&tattr))
+      handle_error("pthread_attr_init");
+    if (0 != pthread_attr_setdetachstate(&tattr, PTHREAD_CREATE_DETACHED))
+      handle_error("pthread_attr_init");
+
     while(1 == 1) {
-      pthread_create(&thread_id, NULL, myThreadFun, NULL);
+      // pass in which iteration this is
+      arg = malloc(sizeof(*arg));
+      if (arg == NULL) {
+        handle_error("malloc");
+      }
+      *arg = i;
+
+      pthread_create(&thread_id, &tattr, myThreadFun, arg);
       if (thread_id >= 0 && i < 655360) {
         i++;
-        printf("After Thread %i\n", i);
+        printf("%i: After Thread Create %i\n", myPID, i);
       } else {
-        printf("Boom\n");
+        printf("%i: Boom\n", myPID);
         break;
       }
     }
+
     exit(0);
 }
